579710841850145d162d638f5c473c288cf9f0b4
Kaushal Patel <artemis@Apollo.local>
2024-10-06 16:53:46 -0400

    feat: dummy search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react'
+
 const SearchBar = (props) => {
-  const { hi } = props
-  return <>{hi}</>
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(searchTerm)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Search Anime List" onChange={handleChange} />
+        <button type="submit">Search</button>
+      </form>
+    </>
+  )
 }
 
 export default SearchBar
