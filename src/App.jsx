8a37363e8265aa3059267446b35b393067b50682
Kaushal Patel <kaushal197@gmail.com>
2024-10-06 17:29:49 -0400

    feat: lift data to external file and implement simple search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import dummyAnimeList from './data/anime'
 import SearchBar from './components/SearchBar'
 import AnimeList from './components/AnimeList'
+import { useState } from 'react'
 
 function App() {
-  const onSearch = (e) => {
-    console.log(e)
+  const [animeList, setAnimeList] = useState(dummyAnimeList)
+
+  const onSearch = (searchTerm) => {
+    console.log(searchTerm)
+    setAnimeList(animeList.filter((anime) => anime.name === searchTerm))
   }
 
   return (
     <>
       <SearchBar onSearch={onSearch} />
-      <AnimeList />
+      <AnimeList animeList={animeList} />
     </>
   )
 }
