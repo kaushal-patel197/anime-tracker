0dcc4f503b8e47629bb6f9e461c0565806ce56c7
Kaushal Patel <kaushal197@gmail.com>
2024-10-06 17:17:19 -0400

    style: AnimeList and AnimeCard

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -1,12 +1,36 @@
+import { styled } from 'styled-components'
+
+const Card = styled.div`
+  border: 0.1rem solid #ddd;
+  padding: 1rem;
+  margin: 1rem;
+  text-align: center;
+  width: 20rem;
+`
+
+const Image = styled.img`
+  max-width: 10rem;
+  height: auto;
+`
+
+const Title = styled.h3`
+  font-size: 2rem;
+  margin: 1rem 0;
+`
+
+const Description = styled.p`
+  font-size: 1.6rem;
+`
+
 const AnimeCard = (props) => {
   const { anime } = props
   return (
     <>
-      <div>
-        <h1>{anime.name}</h1>
-        <img src={anime.image} />
-        <p>{anime.description}</p>
-      </div>
+      <Card>
+        <Title>{anime.name}</Title>
+        <Image src={anime.image} alt={anime.name} />
+        <Description>{anime.description}</Description>
+      </Card>
     </>
   )
 }
--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -1,4 +1,10 @@
+import { styled } from 'styled-components'
 import AnimeCard from './AnimeCard'
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
 
 const AnimeList = (props) => {
   const animeList = [
@@ -10,15 +16,30 @@ const AnimeList = (props) => {
       description:
         'InuYasha is a shōnen action adventure romantic comedy, with elements from the horror genre.',
     },
+    {
+      id: 2,
+      name: 'Yu Yu Hakusho',
+      image:
+        'https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=480,height=720/catalog/crunchyroll/e60626c56bf7437857e0caf7cbc4bf4a.jpg',
+      description:
+        'One fateful day, Yuusuke Urameshi, a 14-year-old delinquent with a dim future, gets a miraculous chance to turn it all around when he throws himself in front of a moving car to save a young boy. ',
+    },
+    {
+      id: 3,
+      name: 'Naruto',
+      image: 'https://cdn.myanimelist.net/images/anime/1565/111305.jpg',
+      description:
+        'It has been two and a half years since Naruto Uzumaki left Konohagakure, the Hidden Leaf Village, for intense training following events which fueled his desire to be stronger.',
+    },
   ]
 
   return (
     <>
-      <div>
+      <ListContainer>
         {animeList.map((anime) => (
-          <AnimeCard anime={anime} />
+          <AnimeCard key={anime.id} anime={anime} />
         ))}
-      </div>
+      </ListContainer>
     </>
   )
 }
