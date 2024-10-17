b4c51eafbac3392bc0af9ff5ae5c6cd053f503c1
Kaushal Patel <kaushal197@gmail.com>
2024-10-16 20:31:15 -0400

    feat: create CRUD functions for watchlist table

--- backend/src/watchlist/watchlist.service.ts
+++ backend/src/watchlist/watchlist.service.ts
@@ -13,4 +13,17 @@ export class WatchlistService {
   async findAll(): Promise<Watchlist[]> {
     return this.watchlistRepository.find()
   }
+
+  async findOne(id: number): Promise<Watchlist> {
+    return this.watchlistRepository.findOneBy({ id })
+  }
+
+  async create(watchlistEntity: Partial<Watchlist>): Promise<Watchlist> {
+    return this.watchlistRepository.save(watchlistEntity)
+  }
+
+  async remove(id: number): Promise<void> {
+    const deletedEntity = await this.watchlistRepository.delete(id)
+    console.log(`Entity Deletd with id ${deletedEntity.affected}`)
+  }
 }
